69d83b7d464206d59bc8c787301192734ed0000f
NewExample <timurmanilov12@mail.ru>
2021-05-25 05:28:23 +0300

    1
    1

--- js/main.js
+++ js/main.js
@@ -6,30 +6,25 @@ $('.catalogButton').on('click', function(e) {
   $('.right').toggleClass('right_active');
 })
 
-$('.cross').on('click', function(e) {
+$('.right').on('click', function(e) {
   e.preventDefault();
   $('.right').toggleClass('right_active');
   $('.menu').toggleClass('menu_active');
 })
 
-$(".catalogButton").mouseenter(function() {
-	// навели курсор на объект (не учитываются переходы внутри элемента)	
-	$('.menu').toggleClass('menu_active');
-  $('.right').toggleClass('right_active');
-});
 
-$(".cross")
-.mouseenter(function(){ 		
-	// отвели курсор с объекта (не учитываются переходы внутри элемента)
-  $('.right').removeClass('right_active');
-  $('.menu').toggleClass('menu_active');
-});
 
 $('.buttonSearchHiden').on('click', function(e) {
   e.preventDefault();
   $('.hideSearch').toggleClass('hide');
   
 })
+
+$(".pUp").click(function() {
+     $('html, body').animate({
+         scrollTop: $(".logoHeader").offset().top
+     }, 1000);
+ })
 
 /**  $(".greyTerritory")
 .mouseenter(function(){ 		
---
+++ js/slick.js
@@ -0,0 +1,5 @@
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 3
+});
